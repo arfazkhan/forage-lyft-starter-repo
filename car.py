875a41bef01dc49e066f3f48772e992357d78895
Arfaz Khan <arfuzkhan@gmail.com>
2023-06-16 15:48:28 +0530

    updated new functionality

--- car.py
+++ car.py
@@ -1,9 +1,10 @@
-class Car:
-    def __init__(self, model, engine, battery, tires):
-        self.model = model
+from serviceable import Serviceable
+
+
+class Car(Serviceable):
+    def __init__(self, engine, battery):
         self.engine = engine
         self.battery = battery
-        self.tires = tires
 
-    def is_due_for_service(self):
-        return self.engine.is_due_for_service() or self.battery.is_due_for_service() or self.tires.is_due_for_service()
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
